28db1bc82c8512146eb526e89de70c66bce3fd6b
Alan Zhang <alan.zhang.jedi@gmail.com>
2022-11-22 00:38:06 -0500

    created weightedAverage, and medianAverage filter functions

--- assignment_part_2.m
+++ assignment_part_2.m
@@ -1,7 +1,30 @@
+% applyMeanFilter creates a plot and wav file based on the filter and returns the filter result
 function meanFilterResult = applyMeanFilter(file, windowSize)
     [y,~] = audioread(file);
     meanFilterData = movmean(y, windowSize);
     plot(meanFilterData)
     saveas(gcf, sprintf('meanFilter%f.png', windowSize))
+    audiowrite('meanFilter.wav', meanFilterData, 60000);
     meanFilterResult = meanFilterData;
+end
+
+% applyWeightedAverageFilter creates a plot and wav file based on the filter and returns the filter result
+function weightedAverageFilterResult = applyWeightedAverageFilter(file, windowSize)
+    [y,~] = audioread(file);
+    b = gausswin(windowSize);
+    weightedAverageFilterData = filter(b, 1, y);
+    plot(weightedAverageFilterData)
+    saveas(gcf, sprintf('weightedAverageFilter%f.png', windowSize))
+    audiowrite('weightedAverageFilter.wav', weightedAverageFilterData, 60000);
+    weightedAverageFilterResult = weightedAverageFilterData;
+end
+
+% applyMedianAverageFilter creates a plot and wav file based on the filter and returns the filter result
+function medianAverageFilter = applyMedianAverageFilter(file, windowSize)
+    [y,~] = audioread(file);
+    medianFilterData = medfilt1(y);
+    plot(medianFilterData)
+    saveas(gcf, sprintf('medianAverageFilter%f.png', windowSize))
+    audiowrite('medianAverageFilter.wav', weightedAverageFilterData, 60000);
+    medianAverageFilterResult = medianFilterData;
 end
